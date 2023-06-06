65809ed13aee4c5265b31253c4a5927c51d4083c
Alexandr Savca <alexandr.savca89@gmail.com>
2023-06-06 07:58:51 +0300

    pathnames.h: add (doxygen) comments

--- pathnames.h
+++ pathnames.h
@@ -1,3 +1,6 @@
+//! \file  pathnames.h
+//! \brief Absolute filenames that seedrng wants for various defaults.
+
 #pragma once
 
 #ifndef LOCALSTATEDIR
@@ -7,3 +10,6 @@
 #define SEED_DIR LOCALSTATEDIR  "/seedrng"
 #define CREDITABLE_SEED         "seed.credit"
 #define NON_CREDITABLE_SEED     "seed.no-credit"
+
+// vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
+// End of file.
