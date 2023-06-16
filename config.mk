825d492eeed8479df6fdf2ede9e86a4421e1915f
Alexandr Savca <alexandr.savca89@gmail.com>
2023-06-16 20:20:31 +0300

    config.mk: indent fix

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # project metadata
-NAME    = seedrng
-VERSION = 0.1
-DIST    = ${NAME}-${VERSION}
+NAME          = seedrng
+VERSION       = 0.1
+DIST          = ${NAME}-${VERSION}
 
 # paths
 PREFIX        = /usr
@@ -9,10 +9,10 @@ MANPREFIX     = ${PREFIX}/share/man
 LOCALSTATEDIR = /var/lib
 
 # flags
-CPPFLAGS = -DLOCALSTATEDIR=\"${LOCALSTATEDIR}\"
-CFLAGS   = -pedantic -Wall -Wextra
-LDFLAGS  = -static
+CPPFLAGS      = -DLOCALSTATEDIR=\"${LOCALSTATEDIR}\"
+CFLAGS        = -pedantic -Wall -Wextra
+LDFLAGS       = -static
 
 # compiler and linker
-CC = cc
-LD = ${CC}
+CC            = cc
+LD            = ${CC}
