f17aa3d05d2808341eb0a2716b0fe1d601205418
Alexandr Savca <alexandr.savca89@gmail.com>
2023-07-08 07:56:16 +0300

    utils.mk: restyle, add longlines target

--- utils.mk
+++ utils.mk
@@ -1,10 +1,12 @@
-GREP_DEFS = --exclude-dir=.git --exclude-dir=.github -R .
+GREPOPT = --exclude-dir=.git --exclude-dir=.github -R .
+FINDOPT = -not \( -path "./.git*" -or -path ".*~" \)
+MAXLINE = 80
 
 urlcodes:
 	@echo "=======> Check URLs for response code"
-	@grep -Eiho "https?://[^\"\\'> ]+" ${GREP_DEFS}  \
-		| xargs -P10 -I{} curl -o /dev/null      \
-		 -sw "[%{http_code}] %{url}\n" '{}'      \
+	@grep -Eiho "https?://[^\"\\'> ]+" ${GREPOPT}  \
+		| xargs -P10 -I{} curl -o /dev/null    \
+		 -sw "[%{http_code}] %{url}\n" '{}'    \
 		| sort -u
 
 podchecker:
@@ -13,12 +15,16 @@ podchecker:
 
 shellcheck:
 	@echo "=======> Check shell scripts for syntax errors"
-	@grep -m1 -l '^#\s*!/bin/sh' ${GREP_DEFS}        \
-		| xargs -L10 shellcheck -s sh
+	@grep -m1 -l '^#\s*!/bin/sh' ${GREPOPT} | xargs -L10 shellcheck -s sh
 
 cppcheck:
 	@echo "=======> Static C/C++ code analysis"
 	@cppcheck --enable=all --check-level=exhaustive  \
-		--suppress=missingIncludeSystem ${CURDIR}
+		--suppress=missingIncludeSystem .
 
-.PHONY: urlcodes podchecker shellcheck cppcheck
+longlines:
+	@echo "=======> Check for long lines (> ${MAXLINE})"
+	@find . -type f ${FINDOPT} -exec awk -v ML=${MAXLINE} \
+		'length > ML { print FILENAME ":" FNR " " $$0 }'  {} \;
+
+.PHONY: urlcodes podchecker shellcheck cppcheck longlines
