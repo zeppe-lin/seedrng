2ec13b1df542bd80b5624e5b6e1680e8859151e1
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-03 04:01:51 +0300

    utils.mk: add file description

--- utils.mk
+++ utils.mk
@@ -1,3 +1,6 @@
+# Makefile.lint is the automated checking of seedrng project
+# for various programmatic and stylistic errors.
+
 all: deadlinks podchecker shellcheck cppcheck flawfinder longlines
 
 deadlinks:
