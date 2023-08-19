83123a18c925c2a66cc6a32702571661ee7cfc32
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-19 16:02:38 +0300

    pathnames.h: cosmetic changes

--- pathnames.h
+++ pathnames.h
@@ -3,9 +3,8 @@
 
 #pragma once
 
-#define SEED_DIR                LOCALSTATEDIR"/seedrng"
-#define CREDITABLE_SEED         "seed.credit"
-#define NON_CREDITABLE_SEED     "seed.no-credit"
+#define SEED_DIR             LOCALSTATEDIR"/seedrng"
+#define CREDITABLE_SEED      "seed.credit"
+#define NON_CREDITABLE_SEED  "seed.no-credit"
 
-// vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
 // End of file.
