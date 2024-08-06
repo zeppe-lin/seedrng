9753bdf57c5465eb9e1d2460ba688e0956720a07
Alexandr Savca <alexandr.savca89@gmail.com>
2024-08-07 00:29:39 +0300

    pathnames.h: comment defines

--- pathnames.h
+++ pathnames.h
@@ -3,8 +3,13 @@
 
 #pragma once
 
+//!< Directory where seed files are stored.
 #define SEED_DIR             LOCALSTATEDIR"/seedrng"
+
+//!< "Creditable" seed file.
 #define CREDITABLE_SEED      "seed.credit"
+
+//!< "Non-creditable" seed file.
 #define NON_CREDITABLE_SEED  "seed.no-credit"
 
 // End of file.
